2c7c1e674da262d195912e75376c9ac01937dc80
Whtery1087 <meloun7@heexy.org>
2024-03-24 13:51:52 +0100

    Add Discord RPC

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::env;
+use discord_rpc_client::{Client, Event};
 
 fn main() {
+    let state_message = env::args().nth(1).unwrap_or_else(|| String::from("Editing: index.js"));
+    let mut drpc = Client::new(1221435094921777222);
+
+    drpc.on_event(Event::Ready, |_ctx| {
+        println!("Discord RPC: READY");
+    });
+
+    drpc.start();
+
+    drpc.set_activity(|act| act.state(state_message))
+        .expect("Failed to set activity");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
