8e686005795c9b811f6420991f5bb3c5b81f7132
Whtery1087 <meloun7@heexy.org>
2024-03-24 21:18:47 +0100

    fix openFile

--- src/functions/openFile.js
+++ src/functions/openFile.js
@@ -1,15 +1,14 @@
-const { ipcRenderer } = require('electron');
-
 function openFile(event) {
     const fileInput = event.target;
     const file = fileInput.files[0];
     const fileName = file.name;
     const reader = new FileReader();
     reader.onload = function(e) {
-        const fileContent = editor.target.result;
+        const fileContent = reader.result;
         const editor = ace.edit("editor");
         editor.setValue(fileContent);
-        ipcRenderer.send('file-opened', fileName);
+        const fileOpenedEvent = new CustomEvent('file-opened', { detail: fileName });
+        document.dispatchEvent(fileOpenedEvent);
     }
     reader.readAsText(file);
     console.log('Název otevřeného souboru:', fileName);
