4b009db63c144bd28b621ba139d02b1cedb3186e
Whtery1087 <meloun7@heexy.org>
2024-03-25 16:45:34 +0100

    ACE Lib update 1.32.8

--- src/functions/editor.js
+++ src/functions/editor.js
@@ -1,7 +1,7 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/chaos");
 editor.getSession().setMode("ace/mode/html");
-editor.renderer.setShowGutter();
+//editor.renderer.setShowGutter();
 editor.getSession().setUseWorker(false);
 editor.setOptions({
     enableBasicAutocompletion: true,
